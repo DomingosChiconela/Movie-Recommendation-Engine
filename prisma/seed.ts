72a402c9bc3b012927a2324ffd44e62541c4b0a8
Domingos chiconela <azevedodomingos78@gmail.com>
2024-10-09 19:07:41 +0200

    feat:Create seed script to automate movie genre creation

---
+++ prisma/seed.ts
@@ -0,0 +1,50 @@
+import {db} from "../src/utils/db.server"
+
+
+
+const main = async () => {
+  const genres = [
+    "ACTION",
+    "ADVENTURE",
+    "COMEDY",
+    "DRAMA",
+    "HORROR",
+    "SCIFI",
+    "ROMANCE",
+    "DOCUMENTARY",
+    "ANIMATION",
+    "FANTASY",
+    "THRILLER",
+    "MYSTERY",
+    "MUSICAL",
+    "BIOGRAPHY",
+    "HISTORICAL",
+    "SPORT",
+    "WAR",
+    "WESTERN",
+    "FAMILY",
+    "CRIME",
+    "NOIR",
+    "SUPERHERO",
+  ];
+
+ 
+  await Promise.all(
+    genres.map((name) => 
+      db.gender.create({
+        data: { name },
+      })
+    )
+  );
+
+  console.log("Genres seeded successfully.");
+};
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await db.$disconnect();
+  });
