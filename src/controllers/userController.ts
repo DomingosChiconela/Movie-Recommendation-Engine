a75d3a45236edb894806d48fe1ba671ada6ec132
Domingos chiconela <azevedodomingos78@gmail.com>
2024-10-11 22:11:35 +0200

    feat:create endpoint to delete user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -287,3 +287,33 @@ export const  updateUser = async(req:Request,res:Response)=>{
         return res.status(500).json({ message: "Internal Server Error" });
     }
 }
+
+
+export const deleteUser = async(req:Request,res:Response)=>{
+    const {userId}= req
+
+    try{
+        const  user = await  db.user.delete({
+            where:{
+                id:userId
+            }
+        })
+
+        if(!user){
+            return  res.status(404).json({message:"user not found"})
+
+        }
+        
+       
+
+        res.status(200).json({message:"user deleted"})
+     
+
+    }
+    catch (error) {
+        console.log(error)
+        
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+  
+}
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,7 +1,7 @@
 
 import  express from "express"
 
-import { register,updateUser,login,getUser} from "../controllers/userController"
+import { register,updateUser,login,getUser,deleteUser} from "../controllers/userController"
 import { AuthMiddleware } from "../middlewares/authMiddleware"
 
 
@@ -12,7 +12,8 @@ export const  userRoute =  express.Router()
 
 userRoute.post("/",register)
 userRoute.post("/login",login)
-userRoute.put("/",AuthMiddleware,updateUser)
 userRoute.get("/",AuthMiddleware,getUser)
+userRoute.put("/",AuthMiddleware,updateUser)
+userRoute.delete("/",AuthMiddleware,deleteUser)
 
 
